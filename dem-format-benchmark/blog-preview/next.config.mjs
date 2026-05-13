06896e1643fc8a5ccfd67578dc8d6aaa029bd8c6
dnf0 <danielfisher0@gmail.com>
2026-05-13 17:36:22 +0100

    fix: add remark-gfm plugin and css styling to render markdown tables correctly

---
+++ dem-format-benchmark/blog-preview/next.config.mjs
@@ -0,0 +1,21 @@
+import createMDX from '@next/mdx'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMath from 'remark-math'
+import remarkGfm from 'remark-gfm'
+import rehypeKatex from 'rehype-katex'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkFrontmatter, remarkMath, remarkGfm],
+    rehypePlugins: [rehypeKatex],
+  },
+})
+
+export default withMDX(nextConfig)
